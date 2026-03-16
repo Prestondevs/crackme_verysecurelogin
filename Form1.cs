2755fbd5cca47d04d49487afa79067df549e8f47
Prestondevs <Prestonvardaman1@gmail.com>
2026-03-16 01:26:49 -0500

    initial commit

---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace crackme_overflow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string correct_user = "braNdon2";
+            string correct_pass = "Leetcoder67";
+            if (Usernamebox.Text == correct_user && Passwordbox.Text == correct_pass)
+            {
+                Form2 success = new Form2();
+                success.Show();
+                this.Hide();
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,30 @@
+﻿using crackme_overflow.Properties;
+using System;
+using System.IO;
+using System.Media;
+using System.Windows.Forms;
+
+namespace crackme_overflow
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
